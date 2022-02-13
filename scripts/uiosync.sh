0abac817ede022774754c4a58814158a2d492cc6
OnionHat <suleymanboyar02@gmail.com>
2022-02-14 00:40:22 +0100

    Misc things like uio cloud storage system

--- scripts/uiosync.sh
+++ scripts/uiosync.sh
@@ -2,6 +2,6 @@
 while inotifywait -r -e modify,create,delete,move ~/dev/uio/; do
     wget -q --spider http://gnu.org
     if [ $? -eq 0 ]; then
-        rsync -avz ~/dev/uio/ uio:~/privat
+        rsync -avz --delete ~/dev/uio/ uio:~/privat
     fi
 done
